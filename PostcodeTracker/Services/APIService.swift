4be75cca5100a4657f450b770041b4533cbeb759
Rick <rahelioheynneman@gmail.com>
2025-05-19 15:59:12 +0100

    updated api service

--- PostcodeTracker/Services/APIService.swift
+++ PostcodeTracker/Services/APIService.swift
@@ -11,7 +11,7 @@ enum APIError: Error {
 
 class APIService {
     static let shared = APIService()
-    private let baseURL = "http://localhost:5000/api"
+    private let baseURL = "http://rickys.ddns.net:5319/api"
     private var authToken: String?
     
     private init() {}
