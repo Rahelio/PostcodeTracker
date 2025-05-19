980c9fbe867a4fd3374c1cf2032e30001c0fe2ee
Rick <rahelioheynneman@gmail.com>
2025-05-19 17:42:16 +0100

    updated server side

--- server/gunicorn_config.py
+++ server/gunicorn_config.py
@@ -6,7 +6,7 @@ backlog = 2048
 
 # Worker processes
 workers = 1  # Reduced to 1 for debugging
-worker_class = 'sync'
+worker_class = 'gevent'  # Changed to gevent for better HTTP/1.1 support
 worker_connections = 1000
 timeout = 30
 keepalive = 2
