54a29019bbb4163797b91fa2869ef82fe9ea2e30
Rick <rahelioheynneman@gmail.com>
2025-06-13 18:35:37 +0100

    updated error message in routes.py

--- routes.py
+++ routes.py
@@ -317,7 +317,7 @@ def start_journey(current_user):
             if not start_postcode:
                 return jsonify({
                     'success': False, 
-                    'message': 'Could not determine UK postcode for your location'
+                    'message': f'Could not determine UK postcode for coordinates ({lat}, {lon}). This app only works within the UK. Please ensure you are in the UK and have a good GPS signal.'
                 }), 400
                 
         except TimeoutError:
